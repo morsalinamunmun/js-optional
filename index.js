00a4633941fdbcc2392364a460e83eab9d3ea57d
morsalinamunmun <morsalinamunmun@gmail.com>
2024-03-24 10:46:09 +0600

    html file connect js file

---
+++ index.js
@@ -0,0 +1,18 @@
+//alert('Hello! look at me')
+
+const alertUser = () =>{
+    alert('Be attentive and serious to invest time for learning!');
+}
+
+const seriousStudent  = () =>{
+    const serious = confirm('Are you serious to invest time for learning!');
+    if(serious === true){
+        alert('well done');
+    }else{
+        alert('do not good');
+    }
+}
+
+const getName = () =>{
+    prompt('What is your name');
+}
